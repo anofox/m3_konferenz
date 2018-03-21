c5f826d739505f0c94d4006633f293bf9b7cf3b8
Simon Müller <simon.mueller@muon-stat.com>
2018-03-22 00:38:24 +0100

    working on LSTM

--- 00_load_smart_meter_data.R
+++ 00_load_smart_meter_data.R
@@ -2,19 +2,59 @@ library(tidyverse)
 library(data.table)
 library(foreach)
 library(iterators)
+library(viridis)
 
 source("r_functions/data_preparation.R")
 
-# Prepare London Smart Meter Data ----
+# 1.0 Prepare London Smart Meter Data ----
 path <- "data/smart-meters-in-london/halfhourly_dataset/"
 dtData <- load_data_bulk(path)
 fst::write_fst(dtData, path = "prepared_data/london_smart_meter/smart_meters_london.fst")
 write_csv(dtData, "prepared_data/london_smart_meter/smart_meters_london.csv")
 
 
-# Add Weather Data ----
+# 2.0 Data Cleaning ----
 fst::read_fst("prepared_data/london_smart_meter/smart_meters_london.fst") %>% 
   as.data.table() -> dtData
+
+# Analyse length
+dtLength <- dtData[, .N, by = id]
+setkey(dtLength, N)
+dtLength
+summary(dtLength)
+p <- AnoFox::heatmapTS(dtData, columnSort = "date_time", columnY = "energy", columnID = "id", doNormalize = T)
+ggsave("images/smart_meter_heatmap.png", p, width = 12, height = 8)
+rm(p)
+
+# filter series not ending in January 2014
+maxDate <- max(dtData$date_time)
+dtData[, tmp := max(date_time), by = "id"]
+dtData <- dtData[tmp == maxDate]
+
+p <- AnoFox::heatmapTS(dtData, columnSort = "date_time", columnY = "energy", columnID = "id", doNormalize = T)
+ggsave("images/smart_meter_heatmap_max_date.png", p, width = 12, height = 8)
+rm(p)
+
+fst::write_fst(dtData, path = "prepared_data/london_smart_meter/smart_meters_london_cleaned.fst")
+write_csv(dtData, "prepared_data/london_smart_meter/smart_meters_london_cleaned.csv")
+
+
+# filter series, s.t. they have the same start point
+dtData[, tmp := min(date_time), by = "id"]
+maxDate <- max(dtData$tmp)
+dtData <- dtData[date_time >= maxDate]
+dtData$tmp <- NULL
+
+p <- AnoFox::heatmapTS(dtData, columnSort = "date_time", columnY = "energy", columnID = "id", doNormalize = T)
+ggsave("images/smart_meter_heatmap_max_date_same_start.png", p, width = 12, height = 8)
+rm(p)
+dtData[, minDate := NULL]
+fst::write_fst(dtData, path = "prepared_data/london_smart_meter/smart_meters_london_cleaned_same_start.fst")
+write_csv(dtData, "prepared_data/london_smart_meter/smart_meters_london_cleaned_same_start.csv")
+a <- read_csv("prepared_data/london_smart_meter/smart_meters_london_cleaned_same_start.csv")
+
+
+# 3.0 Add Weather Data & drop short series ----
 weatherData <- readr::read_csv("data/smart-meters-in-london/weather_hourly_darksky.csv")
 setnames(weatherData, "time", "date_time")
 weatherData <- as.data.table(weatherData)
